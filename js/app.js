6349dae772826f621c2aae635b6390b3e17be8f1
Jacob Flaxman <jacobflaxman1@gmail.com>
2021-03-17 16:29:16 -0700

    tweaking parts

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 (function () {
   let d = document;
+  d.onmousemove = getMousePosition;
 
   let playButton = d.createElement("button");
   playButton.classList.add("play-button");
@@ -7,13 +8,24 @@
   playButton.innerText = "Start";
   playButton.addEventListener("click", () => playTone(0.25));
 
+  let nSlider = d.getElementsByClassName("numParticle-slider");
+  nSlider[0].addEventListener("change", (e) => changeNumParticles(e));
+
   let canvas = d.body.appendChild(d.createElement("canvas"));
   let context = canvas.getContext("2d");
   let time = 0;
   let sin = Math.sin;
   let cos = Math.cos;
+  let tan = Math.tan;
   let PI = Math.PI;
-  let octaveMultiplier = 3;
+  let octaveMultiplier = 5;
+
+  let numParticles = 50000;
+
+  function changeNumParticles(e) {
+    console.log(e.target);
+    numParticles = e.target.value;
+  }
 
   let audioContext = new (window.AudioContext || window.webkitAudioContext)();
   let masterGainNode = audioContext.createGain();
@@ -103,43 +115,44 @@
     // delay.connect(audioContext.destination);
     // gain.connect(masterGainNode);
 
-    let randnow = audioContext.currentTime;
+    let now = audioContext.currentTime;
 
     const rand = Math.random() * 8;
+    const randTime = Math.random() * 2;
 
     if (rand < 1) {
       oscillator1.start();
-      oscillator1.stop(now + 1);
+      oscillator1.stop(rand + 1);
     } else if (rand > 1.5 && rand < 2) {
       oscillator1.start();
       oscillator2.start();
-      oscillator2.stop(rand);
-      oscillator1.stop(rand);
+      oscillator2.stop(now + randTime / 2);
+      oscillator1.stop(now + randTime / 2);
     } else if (rand > 2 && rand < 2.5) {
       oscillator2.start();
       oscillator3.start();
-      oscillator3.stop(rand);
-      oscillator2.stop(rand);
+      oscillator3.stop(now + randTime / 2);
+      oscillator2.stop(now + randTime / 2);
     } else if (rand > 2.5 && rand < 3) {
       oscillator2.start();
-      oscillator2.stop(rand + 1);
+      oscillator2.stop(now + randTime / 2);
     } else if (rand > 3 && rand < 4) {
       oscillator3.start();
-      oscillator3.stop(rand + 1);
+      oscillator3.stop(now + randTime / 2);
     } else if (rand > 4 && rand < 5) {
       oscillator4.start();
-      oscillator4.stop(rand + 1);
+      oscillator4.stop(now + randTime / 2);
     } else if (rand > 5 && rand < 6) {
       oscillator5.start();
-      oscillator5.stop(rand + 1);
+      oscillator5.stop(now + randTime / 2);
     } else if (rand > 6 && rand < 7) {
       oscillator6.start();
-      oscillator6.stop(rand + 1);
+      oscillator6.stop(now + randTime / 2);
       oscillator3.start();
-      oscillator3.stop(rand);
+      oscillator3.stop(now + randTime / 2);
     } else if (rand > 7 && rand < 8) {
       oscillator7.start();
-      oscillator7.stop(rand + 1);
+      oscillator7.stop(now + randTime / 2);
     }
 
     // oscillator1.stop(now + 1);
@@ -149,6 +162,11 @@
     canvas.width = w = innerWidth;
     canvas.height = h = innerHeight;
   }
+
+  resize();
+
+  let mouseX = w / 2;
+  let mouseY = h / 2;
 
   canvas.addEventListener("resize", resize, false);
 
@@ -162,7 +180,6 @@
     context.clearRect(0, 0, w, h);
     context.globalCompositeOperation = "lighter";
     analyser.getByteTimeDomainData(dataArray);
-
     context.fillStyle =
       dataArray[0] === 128
         ? "rgba(0, 255, 255, .7)"
@@ -171,21 +188,62 @@
     time += 0.1;
 
     // The number of particles to generate
-    // i = bufferLength;
-    for (let i = 0; i < bufferLength; i++) {
-      let v = dataArray[i] / 50;
-      let y = v * h;
+    let visualizerFactor = dataArray[0];
+    let factor1 = tan;
+    let factor2 = cos;
+    let factor3 = tan;
+
+    if (visualizerFactor > 130) {
+      factor1 = sin;
+      factor2 = sin;
+      factor3 = sin;
+    } else if (visualizerFactor > 110 && visualizerFactor < 130) {
+      factor1 = tan;
+      factor2 = tan;
+      factor3 = cos;
+    } else if (visualizerFactor > 90 && visualizerFactor < 110) {
+      factor1 = sin;
+      factor2 = sin;
+      factor3 = sin;
+    } else if (visualizerFactor > 70 && visualizerFactor < 90) {
+      factor1 = cos;
+      factor2 = cos;
+      factor3 = cos;
+    } else if (visualizerFactor > 80 && visualizerFactor < 90) {
+      factor1 = tan;
+      factor2 = sin;
+      factor3 = cos;
+    } else if (visualizerFactor < 80) {
+      factor1 = tan;
+      factor2 = cos;
+      factor3 = sin;
+    }
+
+    i = numParticles;
+    while (i--) {
+      let v = dataArray[i] / 35;
+      let y = mouseY;
       r =
         y *
         0.4 *
-        (sin((time + i) * (0.05 + (Math.tan(time * 0.00002) / PI) * 0.2)) / PI);
-
-      context.fillRect(sin(i) * r + w / 2, cos(i) * r + h / 2, 2, 4);
+        (factor1((time + i) * (0.05 + (factor1(time * 0.00002) / PI) * 0.2)) /
+          PI);
+      context.fillRect(
+        factor1(i) * r + mouseX / 10 + w / 2,
+        factor3(i) * r + mouseY / 10 + h / 2,
+        2,
+        4
+      );
     }
   }
 
-  resize();
+  function getMousePosition(e) {
+    mouseX = e.clientX;
+    mouseY = e.clientY;
+    console.log(mouseX, mouseY);
+  }
+
   setInterval(() => drawPattern1(), 20);
-  setInterval(() => playTone(0.25), Math.random() * 10000);
+  setInterval(() => playTone(0.25), Math.random() * 1000);
   // setInterval(() => playTone(Math.random() * 5), Math.random() * 5000);
 })();
